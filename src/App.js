773387c5e0c58c15d06fd4f460ad9e01a5f61879
Vincent <makokhavomondi@gmail.com>
2023-02-27 15:27:56 +0300

    Configure the use of react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import Login from './Pages/Login/Login';
 import HouseCard from './Components/HouseCard/HouseCard';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <HouseCard />
+      
+      <Routes>
+        <Route exact path='/home' element={<Home />} />
+        <Route exact path='/login' element={<Login />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   );
 }
