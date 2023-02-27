60f578454f85f3b306150be33c5a9d601e974438
Vincent <makokhavomondi@gmail.com>
2023-02-27 15:12:21 +0300

    Use HouseCards in homepage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,17 @@
 import React from "react";
+import HouseCard from "../../Components/HouseCard/HouseCard";
 import "./Home.css"
 
 function Home(){
+    const houseCards = []
+
+    for(let i = 0; i < 20; i++){
+        houseCards.push(<HouseCard key={i}/>)
+    }
     return (
         <div id="home">
-            <div className="container">
-
+            <div id="house-cards" className="container">
+                {houseCards}
             </div>
         </div>
     )
