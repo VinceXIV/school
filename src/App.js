15288b20a543635276184cb45d164d92da8e3362
Vincent <makokhavomondi@gmail.com>
2023-02-27 15:51:09 +0300

    :construction: Configure routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+import React, {useEffect, useState} from 'react';
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import Login from './Pages/Login/Login';
-import HouseCard from './Components/HouseCard/HouseCard';
 import Home from './Pages/Home/Home';
 
 function App() {
+  const navigate = useNavigate()
+  const [activePage, setActivePage] = useState(localStorage.getItem('activePage') || "")
+
+  useEffect(()=>{
+    navigate(`/${activePage}`)
+    localStorage.setItem('activePage', activePage)
+  }, [activePage])
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar activePage={activePage} setActivePage={setActivePage}/>
       
       <Routes>
         <Route exact path='/home' element={<Home />} />
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./Navbar.css"
 
-function Navbar(){
+function Navbar({activePage, setActivePage}){
     return (
         <div id="navbar">
             <div className="container">
                 <div id="company-logo">Schooled</div>
                 <ul id="navbar-options">
-                    <li>Log in</li>
+                    <li
+                        className={activePage === "home"? "active": ""}
+                        onClick={()=>setActivePage('home')}
+                        >Home
+                    </li>
+                    <li
+                        className={activePage === "login"? "active": ""}
+                        onClick={()=>setActivePage('login')}
+                        >Log in
+                    </li>
                     <li>Option 2</li>
                     <li>Option 3</li>
                 </ul>
